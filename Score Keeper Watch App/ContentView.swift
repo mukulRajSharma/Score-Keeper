ab9c69f658215ccb24b3a79bd738f691b5e5b4b2
Mukul Sharma <mukul02015@gmail.com>
2025-04-04 00:54:31 +1100

    add limits and re-usable counter

--- Score Keeper Watch App/ContentView.swift
+++ Score Keeper Watch App/ContentView.swift
@@ -7,50 +7,47 @@
 
 import SwiftUI
 
+
+struct Counter: View {
+    @State var counter: Int
+    let minValue: Int
+    let maxValue: Int
+    
+    var body: some View {
+        VStack(spacing:5) {
+            Button(action: { counter += 1 }) {
+                Image(systemName: "plus")
+                    .scaleEffect(1)
+                    .foregroundColor(.green)
+            }
+            .disabled(counter == maxValue)
+            // Display the counter value
+            Text("\(counter)")
+                .font(.system(size: 50, design: .rounded))
+            // Decrement Counter
+            Button(action: { counter -= 1 }) {
+                Image(systemName: "minus")
+                    .scaleEffect(1)
+                    .foregroundColor(.red)
+            }
+            .disabled(counter == minValue)
+            
+        }
+        .padding(.top, 20)
+    }
+    
+}
 //@main
 struct ContentView: View {
     @AppStorage("counterOne") private var counterOne = 0
     @AppStorage("counterTwo") private var counterTwo = 0
-
+    let minValue = 0
+    let maxValue = 21
+    
     var body: some View {
         HStack {
-            VStack(spacing:5) {
-                Button(action: { counterOne += 1 }) {
-                    Image(systemName: "plus")
-                        .scaleEffect(1)
-                        .foregroundColor(.green)
-                }
-                // Display the counter value
-                Text("\(counterOne)")
-                    .font(.system(size: 50, design: .rounded))
-                // Decrement Counter
-                Button(action: { counterOne -= 1 }) {
-                    Image(systemName: "minus")
-                        .scaleEffect(1)
-                        .foregroundColor(.red)
-                }
-                
-            }
-            .padding(.top, 20)
-            
-            VStack(spacing:5) {
-                Button(action: { counterTwo += 1 }) {
-                    Image(systemName: "plus")
-                        .scaleEffect(1)
-                        .foregroundColor(.green)
-                }
-                // Display the counter value
-                Text("\(counterTwo)")
-                    .font(.system(size: 50, design: .rounded))
-                // Decrement Counter
-                Button(action: { counterTwo -= 1 }) {
-                    Image(systemName: "minus")
-                        .scaleEffect(1)
-                        .foregroundColor(.red)
-                }
-                
-            }
-            .padding(.top, 20)
+            Counter(counter: counterOne, minValue: minValue, maxValue: maxValue)
+            Counter(counter: counterTwo, minValue: minValue, maxValue: maxValue)
         }
         .padding()
             
